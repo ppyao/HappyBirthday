d492461f9216c7daecba64f30c0a9701db9a3018
ppyao <dearyooyoo@gmail.com>
2016-01-21 18:19:20 -0800

    Allow adding whipped cream / chocolate; Allow input name

--- app/src/main/java/com/example/yao/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/yao/happybirthday/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.yao.happybirthday;
 
 import android.os.Bundle;
         import android.support.v7.app.ActionBarActivity;
-        import android.view.View;
-        import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
-    int quantity = 2;
+public class MainActivity extends AppCompatActivity {
+    int quantity = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +28,45 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity *5;
-        String priceMessage = "Amount due $" + price;
-        priceMessage = priceMessage + "\nThank you!";
+        EditText nameEditText = (EditText) findViewById(R.id.name_edittext);
+        String name = nameEditText.getText().toString();
+
+        CheckBox creamCheckBox = (CheckBox) findViewById(R.id.cream_checkbox);
+        boolean hasWhippedCream = creamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+        Log.v("MainActivity", "Whipped cream: " + hasWhippedCream);
         displayMessage(priceMessage);
+    }
+
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+        int basePrice = 5;
+        if (hasWhippedCream) basePrice++;
+        if (hasChocolate) basePrice += 2;
+        return quantity *basePrice;
+    }
+
+    /**
+     * Create summary of the order.
+     *
+     * @param name of the user
+     * @param hasWhippedCream is whether or not the user wants whipped cream topping
+     * @param hasChocolate is whether or not the user wants whipped cream topping
+     * @param price of the order
+     * @return text summary
+     */
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String priceMessage = "Name: " + name;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nAdd whipped cream? " + hasWhippedCream;
+        priceMessage += "\nAdd chocolate? " + hasChocolate;
+        priceMessage += "\nAmount due $" + price;
+        priceMessage = priceMessage + "\nThank you!";
+        return priceMessage;
     }
 
     /**
@@ -38,13 +78,21 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void increment(View view) {
-        quantity++;
-        display(quantity);
+        if (quantity < 100) {
+            quantity++;
+            display(quantity);
+        }
+        else {
+            Toast.makeText(this, "You can not have more than 100 cups of coffee", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void decrement(View view) {
-        quantity--;
-        display(quantity);
+        if (quantity > 1) {
+            quantity--;
+            display(quantity);
+        }
+        else Toast.makeText(this, "You can not have less than 1 cup of coffee", Toast.LENGTH_SHORT).show();
     }
 
     /**
