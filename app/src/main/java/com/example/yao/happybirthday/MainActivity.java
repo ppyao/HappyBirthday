114034aa4068eaa1c96e0ff11f5e9ba80d728389
ppyao <dearyooyoo@gmail.com>
2016-01-21 23:03:31 -0800

    Add simple stying

--- app/src/main/java/com/example/yao/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/yao/happybirthday/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.yao.happybirthday;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
         import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -41,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
         String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
         Log.v("MainActivity", "Whipped cream: " + hasWhippedCream);
         displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        //intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(intent.EXTRA_TEXT, priceMessage);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee order for " + name);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
